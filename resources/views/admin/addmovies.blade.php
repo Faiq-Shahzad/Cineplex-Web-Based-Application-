5dd6dd4d32d6a41603067efe7a51a07803435786
Faiq Shahzad <f03315558407@gmail.com>
2022-01-04 19:37:19 +0500

    Cineplex Admin Add Movies Form

--- resources/views/admin/addmovies.blade.php
+++ resources/views/admin/addmovies.blade.php
@@ -1,33 +1,43 @@
 @extends('layouts.adminlayout')
 
+<style>
+     h1{
+        margin-top: 2% !important;
+        border-bottom: 1px solid white;
+        font-weight: bold !important;
+    }
+
+    hr{
+        color: white !important;
+        margin-top: 0 !important;
+        padding: 0 !important;
+    }
+
+    form{
+        color: white;
+    }
+</style>
+
 @section('content')
-<div>
+    
 
     
 	<form enctype="multipart/form-data" action="{{ url('/addMovies') }}" method="POST">
         @csrf
 
+    <!-- ----------------------------------- ADD MOVIE FORM ----------------------------------- -->
 		<div class="row form-group">
-
-			<h1 class="col-sm-12"><i class="fas fa-university"></i> Student Entry Form</h1>
-
-		</div>
-
-    <!-- ----------------------------------- PERSONAL INFORMATION ----------------------------------- -->
-
-
-		<div class="row form-group">
-
-			<h4 class="col-sm-12">Personal Information</h4>
-
+            <div class="col-xl-6 offset-xl-3">
+                <h1><i class="fas fa-film"></i> Add New Movies</h1>
+            <hr>
+            </div>
 		</div>
 
 
 		<div class="row form-group">
-
-			<label class="col-sm-2 col-form-label">Name</label>
+			<label class="col-xl-2 col-form-label">Movie Name</label>
 			<div class="col-sm-10">
-				<input type="text" id="name" class="form-control" name="movie_name" placeholder="Faiq"><br>
+				<input type="text" id="name" class="form-control" name="movie_name" placeholder="Spiderman: Home Coming"><br>
 			</div>
 
 		</div>
@@ -35,19 +45,21 @@
 
 		<div class="row form-group">
 			
-			<label class="col-sm-2 col-form-label">Father Name</label>				
+			<label class="col-sm-2 col-form-label">Movie Description</label>				
 			<div class="col-sm-10">
-				<input type="text" id="f_name" class="form-control" name="movie_description" placeholder="Shahzad"><br>
+                <textarea name="movie_description" id="" cols="30" rows="7" id="description" class="form-control" 
+                placeholder="A young Peter Parker/Spider-Man begins to navigate his newfound identity as the web-slinging super hero Spider-Man. Thrilled by his experience with the Avengers, Peter returns home, where he lives with his Aunt May, under the watchful eye of his new mentor Tony Stark. Peter tries to fall back into his normal daily routine - distracted by thoughts of proving himself to be more than just your friendly neighborhood Spider-Man - but when the Vulture emerges as a new villain, everything that Peter holds most important will be threatened.
+                "></textarea><br>
+				{{-- <input type="text" id="description" class="form-control" name="movie_description" placeholder="Spider-man has returned with a new actor Tom Holland"> --}}
 			</div>
-
 		</div>
 
 
 		<div class="row form-group">
 
-			<label class="col-sm-2 col-form-label">Age</label>
+			<label class="col-sm-2 col-form-label">Ratings</label>
 			<div class="col-sm-2">
-				<input type="number" id="age" class="form-control" name="ratings" placeholder="20"><br>
+				<input type="number" id="ratings" class="form-control" name="ratings" placeholder="9"><br>
 			</div>
 
 		</div>
@@ -55,19 +67,16 @@
 
 		<div class="row form-group">
 
-			<label class="col-sm-2 col-form-label">Email Address</label>
+			<label class="col-sm-2 col-form-label">Year of Release</label>
 			<div class="col-sm-3">
-				<input type="number" id="mail" class="form-control" name="year" placeholder="faiq@something.com"><br>
+				<input type="number" id="year" class="form-control" name="year" placeholder="2021"><br>
 			</div>
 
 		</div>
-		
-		
-
 
 		<div class="row form-group">
 			
-			<label class="col-sm-2 col-form-label" for="img">Your Picture</label>
+			<label class="col-sm-2 col-form-label" for="img">Cover Picture</label>
 			<div class="col-sm-3">
 				<input type="file" id="img" class="form-control" name="movie_cover"><br>
 			</div>
@@ -89,6 +98,4 @@
 	</form>
 
 	<br>
-</div>
-    
 @endsection
